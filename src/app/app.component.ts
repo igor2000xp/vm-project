1fa04507e7cc2041837f14c87792a3b4aae1224d
igor2000xp <igorring@gmail.com>
2024-11-25 00:51:52 +0100

    feat: create standalone structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDividerModule,
+    CommonModule,
+    // BrowserAnimationsModule,
+    // BrowserModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
 })
 export class AppComponent {
   title = 'gallery-template';
 }
+
+// HeaderComponent
